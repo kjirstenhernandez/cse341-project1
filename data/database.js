68d12b009303c847a2231b23ca644dc59544e4d1
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-09-20 21:54:29 -0600

    Added db functions and exports

---
+++ data/database.js
@@ -0,0 +1,31 @@
+const { MongoOIDCError } = require('mongodb');
+
+const dotenv = require('dotenv').config();
+
+const MongoClient = require('mongo').MongoClient
+
+let database;
+
+const intDb = (callback) => {
+    if (database) {
+        console.log('DB is already initialized!');
+        return callback(null, database);
+    }
+    MongoClient.connect(process.env.MONGODB_URI)
+        .then((client) => {
+            database = client;
+            callback(null, database);
+        })
+        .catch((err) => {
+            callback(err)
+        });
+};
+
+const getDatabase = () => {
+    if (!database) {
+        throw Error('Database not initialized')
+    }
+    return database;
+};
+
+module.exports = { initDb, getDatabase }
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
-
+const mongodb = require('/data/database');
 const port = process.env.PORT || 3000;
 
 app.use("/", require("./routes"));
 
-app.listen(port, () => {console.log(`Listening on port ${port}`)});
+mongodb.initDB((err) => {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        app.listen(port, () => {console.log(`Listening on port ${port}`)});
+    }
+})
+

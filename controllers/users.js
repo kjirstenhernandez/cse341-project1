4b8557019610d2bf8e05acd3e846cdf52959dc04
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-09-21 19:34:48 -0600

    fixed errors for db connection

--- controllers/users.js
+++ controllers/users.js
@@ -2,20 +2,21 @@ const mongodb = require("../data/database");
 const ObjectId = require('mongodb').ObjectId; //primary key for MongoDB
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDatabase().db('Testing').collection('users').find();
+    const result = await mongodb.getDatabase().db().collection('Contacts').find();
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users);
     })
 };
+
 const getSingle = async (req, res) => {
-    const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db("Testing").collection('users').find({_id});
+    const userId = new ObjectId.createFromHexString(req.params.id);
+    const result = await mongodb.getDatabase().db().collection('Users').find({_id: userId});
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users[0]);
-    })
+    });
 };
 
 
-module_exports = { getAll, getSingle }
+module.exports = { getAll, getSingle }
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const usersController = require('../controllers/users');  d
+const usersController = require('../controllers/users');  
 
 router.get('/', usersController.getAll);
-router.get('/:id', usersController.getSingle)
+router.get('/:id', usersController.getSingle);
+
+module.exports= router;

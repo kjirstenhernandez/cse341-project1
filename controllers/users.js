8b9f912057c09f08f76ddfd04568ed4cc1d72a56
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-09-25 20:53:55 -0600

    Added deleteUser and updateUser + routes

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const mongodb = require("../data/database");
-const ObjectId = require('mongodb').ObjectId; //primary key for MongoDB
+const ObjectId = require('mongodb'); //primary key for MongoDB, only if you're using Mongo's generated ID
 
 const getAll = async (req, res) => {
     const result = await mongodb.getDatabase().db().collection('Contacts').find();
@@ -10,13 +10,58 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    const userId = new ObjectId.createFromHexString(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('Users').find({_id: userId});
+    const userId = req.params.id;
+    const result = await mongodb.getDatabase().db().collection('Contacts').find({_id: userId});
     result.toArray().then((users) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(users[0]);
     });
 };
 
+const createUser = async (req, res) => {
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    }
+    const response = await mongodb.getDatabase().db().collection('Contacts').insertOne(user);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while creating the user')
+    }
+}
 
-module.exports = { getAll, getSingle }
+const updateUser = async (req, res) => {
+    const userId = (req.params.id);
+    const user = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    }
+
+    const response = await mongodb.getDatabase.db().collection('Contacts').updateOne({_id: userId}, user);
+    if (response.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while updating the user')
+    }
+}
+
+const deleteUser = async (req, res) => {
+    const userId = (req.params.id);
+    const response = await mongodb.getDatabase.db().collection('Contacts').deleteOne({_id: userId});
+
+    if (response.deletedCount > 0){
+        res.status(204).send();
+    } else {
+        res.status(500).json(response.error || 'Some error occurred while deleting the user') 
+    }
+}
+
+
+module.exports = { getAll, getSingle, createUser, updateUser, deleteUser }
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const mongodb = require('./data/database');
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
 
-
+//Place bodyparser BEFORE the routes, otherwise routes tries to process first!
+app.use(bodyParser.json());
 // ROUTES
 app.use("/", require("./routes"));
 
